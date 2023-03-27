a0a8ec2f4ab92d9c0b6e3e6559f0fb1ca1407612
jhionisio <mr.joaomarcelo@gmail.com>
2023-03-27 15:31:16 -0300

    DONE

--- src/main/java/com/checkpoint/um/Main.java
+++ src/main/java/com/checkpoint/um/Main.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TesteCP");
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
